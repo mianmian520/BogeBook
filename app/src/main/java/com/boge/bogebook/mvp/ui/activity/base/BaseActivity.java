587ff3a62975654f8bab06d949985dd56fbf95f6
mianmian520 <75439601@qq.com>
2016-10-14 18:25:40 +0800

    添加Activity的基类

---
+++ app/src/main/java/com/boge/bogebook/mvp/ui/activity/base/BaseActivity.java
@@ -0,0 +1,55 @@
+package com.boge.bogebook.mvp.ui.activity.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.boge.bogebook.BookApplication;
+import com.boge.bogebook.component.ActivityComponent;
+import com.boge.bogebook.component.DaggerActivityComponent;
+import com.boge.bogebook.module.ActivityModule;
+
+import butterknife.ButterKnife;
+
+/**
+ * @author boge
+ * @version 1.0
+ * @date 2016/10/13
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected ActivityComponent activityComponent;
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initInjector();
+
+    protected abstract void initViews();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initActivityComponent();
+
+        int layoutId = getLayoutId();
+        setContentView(layoutId);
+
+        initInjector();
+
+        ButterKnife.bind(this);
+        initViews();
+
+    }
+
+    private void initActivityComponent() {
+        activityComponent = DaggerActivityComponent.builder()
+                .applicationComponent(((BookApplication) getApplication()).getmApplicationComponent())
+                .activityModule(new ActivityModule(this))
+                .build();
+    }
+
+    public ActivityComponent getActivityComponent() {
+        return activityComponent;
+    }
+}
